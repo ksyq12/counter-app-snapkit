73cde91dcb8c7c3c9056a4b9da62cea3e3168984
blueberry35 <ksyq12@daum.net>
2025-03-26 17:44:06 +0900

    Feat: Implement plus, minus, and reset buttons

--- CounterAppSnapkitStoryboard/CounterAppSnapkitStoryboard/ViewController.swift
+++ CounterAppSnapkitStoryboard/CounterAppSnapkitStoryboard/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    private var counter: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        numberLabel.text = "\(counter)"
     }
-
-
+    
+    @IBAction func minusButtonTapped(_ sender: UIButton) {
+        counter -= 1
+        numberLabel.text = "\(counter)"
+    }
+    
+    @IBAction func plusButtonTapped(_ sender: UIButton) {
+        counter += 1
+        numberLabel.text = "\(counter)"
+    }
+    
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+        counter = 0
+        numberLabel.text = "\(counter)"
+    }
 }
 
